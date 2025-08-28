312ab10880d2784a50cc29162857951311089309
rohro1 <rohanrao537@gmail.com>
2025-08-28 20:17:28 +0000

    Force update all files

--- main.py
+++ main.py
@@ -1,24 +1,45 @@
-from flask import Flask, redirect, request, url_for
-from src.utils.oauth_drive import create_auth_url, fetch_and_store_credentials, get_drive_service
+from flask import Flask, request, redirect, render_template_string
+from src.utils.oauth_drive import fetch_and_store_credentials, get_drive_service
+from src.utils.status_tracker import get_all_statuses
 
 app = Flask(__name__)
-SCOPES = ["https://www.googleapis.com/auth/drive.file"]
-CREDENTIALS_FILE = "credentials.json"
 
+CREDENTIALS_FILE = "credentials.json"
+SCOPES = ["https://www.googleapis.com/auth/drive"]
+REDIRECT_URI = "https://tiktok-livestream-recorder.onrender.com/oauth2callback"
+
+# Homepage
 @app.route("/")
 def index():
-    return '<a href="/authorize">Authorize Google Drive Access</a>'
+    auth_url = f"https://accounts.google.com/o/oauth2/auth?client_id=<YOUR_CLIENT_ID>&redirect_uri={REDIRECT_URI}&scope={' '.join(SCOPES)}&response_type=code&access_type=offline&prompt=consent"
+    return f'<h2>Authorize Google Drive Access</h2><a href="{auth_url}">Click here to authorize</a>'
 
-@app.route("/authorize")
-def authorize():
-    auth_url = create_auth_url(CREDENTIALS_FILE, SCOPES)
-    return redirect(auth_url)
-
-@app.route("/callback")
-def callback():
+# OAuth callback route
+@app.route("/oauth2callback")
+def oauth2callback():
     request_url = request.url
-    creds = fetch_and_store_credentials(CREDENTIALS_FILE, SCOPES, request_url)
-    return "Authorization successful! You can close this window."
+    fetch_and_store_credentials(
+        credentials_file=CREDENTIALS_FILE,
+        scopes=SCOPES,
+        redirect_uri=REDIRECT_URI,
+        request_url=request_url
+    )
+    # After successful auth, redirect to status page
+    return redirect("/status")
+
+# Status page
+@app.route("/status")
+def status():
+    all_statuses = get_all_statuses()
+    html = "<h1>TikTok Livestream Status</h1>"
+    if not all_statuses:
+        html += "<p>No data yet.</p>"
+    else:
+        html += "<table border='1' cellpadding='5'><tr><th>Username</th><th>Online</th><th>Current Duration (s)</th><th>Last Online</th></tr>"
+        for user, info in all_statuses.items():
+            html += f"<tr><td>{user}</td><td>{info['online']}</td><td>{info['current_duration']}</td><td>{info['last_online']}</td></tr>"
+        html += "</table>"
+    return render_template_string(html)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
--- src/utils/status_tracker.py
+++ src/utils/status_tracker.py
@@ -1,31 +1,12 @@
-import threading
-from datetime import datetime, timedelta
+# A simple in-memory tracker for user livestream statuses
+statuses = {}
 
-class StatusTracker:
-    def __init__(self):
-        self._lock = threading.Lock()
-        self._status = {}
+def update_status(username, online=False, current_duration=0, last_online=None):
+    statuses[username] = {
+        "online": online,
+        "current_duration": current_duration,
+        "last_online": last_online
+    }
 
-    def update_status(self, username, online=False, recording_time=None):
-        with self._lock:
-            if username not in self._status:
-                self._status[username] = {
-                    "online": False,
-                    "current_recording": None,
-                    "last_online": None,
-                    "last_duration": None
-                }
-            user_status = self._status[username]
-            user_status["online"] = online
-            if online and recording_time is not None:
-                user_status["current_recording"] = str(recording_time)
-            elif not online:
-                # Save last recording duration
-                if user_status["current_recording"] is not None:
-                    user_status["last_duration"] = user_status["current_recording"]
-                user_status["current_recording"] = None
-                user_status["last_online"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
-    def get_all_status(self):
-        with self._lock:
-            return dict(self._status)
+def get_all_statuses():
+    return statuses
