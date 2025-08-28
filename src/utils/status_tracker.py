6df933d69a980a455f74dd9b50dfa03f368aab63
rohro1 <rohanrao537@gmail.com>
2025-08-28 23:27:16 +0000

    Force update all files

--- src/utils/status_tracker.py
+++ src/utils/status_tracker.py
@@ -1,19 +1,47 @@
+# src/utils/status_tracker.py
+
 import threading
+from datetime import datetime
 
 class StatusTracker:
     def __init__(self):
-        self._lock = threading.Lock()
-        self._data = {}
+        self.lock = threading.Lock()
+        self.users = {}
 
-    def update_user(self, username, online=False, recording_duration=0, last_online=None, live_duration=0):
-        with self._lock:
-            self._data[username] = {
-                "online": online,
-                "recording_duration": recording_duration,
-                "last_online": last_online,
-                "live_duration": live_duration
-            }
+    def init_users(self, usernames):
+        with self.lock:
+            for username in usernames:
+                if username not in self.users:
+                    self.users[username] = {
+                        "online": False,
+                        "recording_duration": 0,
+                        "last_online": None,
+                        "live_duration": 0
+                    }
+
+    def update_status(self, username, online):
+        with self.lock:
+            if username not in self.users:
+                self.init_users([username])
+            user = self.users[username]
+            now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            if online:
+                if not user["online"]:
+                    user["last_online"] = now
+                    user["recording_duration"] = 0
+                    user["live_duration"] = 0
+                user["online"] = True
+                user["recording_duration"] += 1
+                user["live_duration"] += 1
+            else:
+                user["online"] = False
+                if user["last_online"] is None:
+                    user["last_online"] = now
 
     def get_status(self):
-        with self._lock:
-            return self._data.copy()
+        with self.lock:
+            return self.users.copy()
+
+
+# Create a singleton instance to use in main.py
+status_tracker = StatusTracker()
