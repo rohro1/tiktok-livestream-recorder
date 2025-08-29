0d6df5dedcb273a1f6c41e80701116c251b45ded
rohro1 <rohanrao537@gmail.com>
2025-08-29 00:21:34 +0000

    Force update all files

--- src/utils/status_tracker.py
+++ src/utils/status_tracker.py
@@ -1,47 +1,33 @@
 # src/utils/status_tracker.py
 
-import threading
-from datetime import datetime
-
 class StatusTracker:
     def __init__(self):
-        self.lock = threading.Lock()
-        self.users = {}
+        self._statuses = {}
 
-    def init_users(self, usernames):
-        with self.lock:
-            for username in usernames:
-                if username not in self.users:
-                    self.users[username] = {
-                        "online": False,
-                        "recording_duration": 0,
-                        "last_online": None,
-                        "live_duration": 0
-                    }
+    def update(self, username, last_online=None, live_duration=None, online=None, recording_duration=None):
+        if username not in self._statuses:
+            self._statuses[username] = {
+                "last_online": last_online or "N/A",
+                "live_duration": live_duration or 0,
+                "online": online or False,
+                "recording_duration": recording_duration or 0
+            }
+        else:
+            if last_online is not None:
+                self._statuses[username]["last_online"] = last_online
+            if live_duration is not None:
+                self._statuses[username]["live_duration"] = live_duration
+            if online is not None:
+                self._statuses[username]["online"] = online
+            if recording_duration is not None:
+                self._statuses[username]["recording_duration"] = recording_duration
 
-    def update_status(self, username, online):
-        with self.lock:
-            if username not in self.users:
-                self.init_users([username])
-            user = self.users[username]
-            now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            if online:
-                if not user["online"]:
-                    user["last_online"] = now
-                    user["recording_duration"] = 0
-                    user["live_duration"] = 0
-                user["online"] = True
-                user["recording_duration"] += 1
-                user["live_duration"] += 1
-            else:
-                user["online"] = False
-                if user["last_online"] is None:
-                    user["last_online"] = now
+    def get_statuses(self):
+        return self._statuses
 
-    def get_status(self):
-        with self.lock:
-            return self.users.copy()
+    def get(self, username, default=None):
+        return self._statuses.get(username, default)
 
 
-# Create a singleton instance to use in main.py
+# instantiate a single global tracker
 status_tracker = StatusTracker()
