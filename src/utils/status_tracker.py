8f37c8b87444258162ce4565f1646f2867333c35
rohro1 <rohanrao537@gmail.com>
2025-08-30 01:20:29 +0000

    Force update all files

--- src/utils/status_tracker.py
+++ src/utils/status_tracker.py
@@ -5,7 +5,7 @@ from datetime import datetime
 class StatusTracker:
     def __init__(self):
         self._lock = threading.RLock()
-        self._map = {}
+        self._map = {}  # username -> dict
 
     def _ensure(self, username):
         with self._lock:
@@ -21,16 +21,19 @@ class StatusTracker:
 
     def get_status(self, username):
         with self._lock:
-            return dict(self._ensure(username))
+            self._ensure(username)
+            return dict(self._map[username])
 
     def update_status(self, username, online=None, live_duration=None, recording=None):
         with self._lock:
             st = self._ensure(username)
             now = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
             if online is not None:
-                st["online"] = online
                 if online:
+                    st["online"] = True
                     st["last_online"] = now
+                else:
+                    st["online"] = False
             if live_duration is not None:
                 st["live_duration"] = live_duration
             if recording is not None:
@@ -45,3 +48,9 @@ class StatusTracker:
         with self._lock:
             st = self._ensure(username)
             return st.get("recording_file")
+
+    # ✅ Fix for your error
+    def get_all(self):
+        """Return a shallow copy of all user statuses"""
+        with self._lock:
+            return {username: dict(info) for username, info in self._map.items()}
