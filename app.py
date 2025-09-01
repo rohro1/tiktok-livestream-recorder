cbf27728e467533a49338b5a00cb5cb00b13bff0
rohro1 <rohanrao537@gmail.com>
2025-09-01 01:33:59 +0000

    Force update all files

--- app.py
+++
@@ -1,7 +0,0 @@
-import os
-from main import app
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    print(f"Starting server on port {port}")
-    app.run(host="0.0.0.0", port=port)
